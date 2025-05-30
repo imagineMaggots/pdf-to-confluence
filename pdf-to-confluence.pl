05345cf50a047c3e6584d8f14d0a4b56ab0987b3
Hendrik <hendriktheede@gmail.com>
2025-05-30 10:31:29 +0200

    Update pdf-to-confluence.pl

--- pdf-to-confluence.pl
+++ pdf-to-confluence.pl
@@ -1,10 +1,18 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-# use REST::Client;
-
-my $test = $ARGV[0];
-
-# my $rest_client = REST::Client->new();
-
-print "Hello World '$test'";
+ 
+my ($name, $number) = @ARGV;
+ 
+if (not defined $name) {
+  die "Need name\n";
+}
+ 
+if (defined $number) {
+  print "Save '$name' and '$number'\n";
+  # save name/number in database
+  exit;
+}
+ 
+print "Fetch '$name'\n";
+# look up the name in the database and print it out
