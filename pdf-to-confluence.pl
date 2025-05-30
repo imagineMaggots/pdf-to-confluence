4722a39e22018e77c0e99a71a43779fdd2e49669
Hendrik <hendriktheede@gmail.com>
2025-05-30 10:21:25 +0200

    Update pdf-to-confluence.pl

--- pdf-to-confluence.pl
+++ pdf-to-confluence.pl
@@ -1,12 +1,12 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use REST::Client;
+# use REST::Client;
 
-my ($url,$token) = @ARGV
-print "$url";
-print "$token";
+my ($url, $token) = @ARGV
+print "URL = '$url'";
+print "Token = '$token'";
 
-my $rest_client = REST::Client->new();
+# my $rest_client = REST::Client->new();
 
 print "Hello World";
