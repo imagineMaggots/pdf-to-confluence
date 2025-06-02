a08d007a4b43b214dbf12632a6b63aeb594076eb
Hendrik <hendriktheede@gmail.com>
2025-06-02 13:41:40 +0200

    Update pdf-to-confluence.py

--- pdf-to-confluence.py
+++ pdf-to-confluence.py
@@ -2,8 +2,6 @@
 import sys
 from atlassian import Confluence
 
-
-location = "~/"
 pdf = "Test.pdf"
 
 confluence = Confluence(url=str(sys.argv[1]), username=str(sys.argv[2]), password=str(sys.argv[3]))
@@ -11,4 +9,4 @@ confluence = Confluence(url=str(sys.argv[1]), username=str(sys.argv[2]), passwor
 print(str(sys.argv))
 
 test = confluence.update_or_create(parent_id=2195457, title="Test", body="Test")
-test = confluence.attach_file(filename=location,name=pdf,page_id=2195457)
+test = confluence.attach_file(name=pdf,page_id=2195457)
