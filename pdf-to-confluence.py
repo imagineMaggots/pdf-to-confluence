fca8dfc5d7b7a9800a91c1e42d4f85014c893b39
Hendrik <hendriktheede@gmail.com>
2025-06-02 14:42:52 +0200

    Update pdf-to-confluence.py

--- pdf-to-confluence.py
+++ pdf-to-confluence.py
@@ -8,18 +8,23 @@ confluence = Confluence(url=str(sys.argv[1]), username=str(sys.argv[2]), passwor
 
 print(str(sys.argv))
 
-test = confluence.update_or_create(parent_id=2195457, title="Test", body="Test")
-
+test = confluence.update_or_create(parent_id=2195457, title="Test", body="")
 page = confluence.get_page_id(space="PDFTOCONFL", title = "Test")
 
 print(str(page))
-
 test = confluence.attach_file(filename="./Test.pdf",name="Test.pdf",page_id=page,content_type="application/pdf")
 
 appending = f"""<p>
+  <h2>Link zur PDF</h1>
   <ac:link>
     <ri:attachment ri:filename="Test.pdf"/>
   </ac:link>
+
+  <h2>Eingebettete PDF</h2>
+  <object data="Test.pdf" type="application/pdf" width="100%" height="500px">
+      <p>Unable to display PDF file.</p>
+  </object>
+  
 </p>"""
 
 confluence.append_page(page, "Test", appending)
