a4946c8846df186fcafa769c9b613e896cf9a4cc
Hendrik <hendriktheede@gmail.com>
2025-06-02 13:08:54 +0200

    Update pdf-to-confluence.py

--- pdf-to-confluence.py
+++ pdf-to-confluence.py
@@ -2,14 +2,16 @@
 import sys
 from atlassian import Confluence
 
-confluence = Confluence(
-                            url=str(sys.argv[1]),
-                            username=str(sys.argv[2]),
-                            password=str(sys.argv[3])
-                        )
+
+confluence = Confluence(url=str(sys.argv[1]), username=str(sys.argv[2]), password=str(sys.argv[3]))
 
 print(str(sys.argv))
 
-test = confluence.create_page(space="DEMO", title = "Test Titel", body = "Test Körper")
+test = confluence.update_page(
+    parent_id=None,
+    page_id=2195457,
+    title="This is the new title",
+    body="This is the new body",
+)
 
 print(test)
